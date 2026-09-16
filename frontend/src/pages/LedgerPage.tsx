6da6f62fbcad2c37830964174dc79fe3f0037bf0
Soronika Pinky <spinkey@schertech.com>
2026-09-17 01:12:05 +0600

    minor update

--- frontend/src/pages/LedgerPage.tsx
+++ frontend/src/pages/LedgerPage.tsx
@@ -18,7 +18,6 @@ import { usePageHeader } from '@/hooks/usePageHeader'
 import { usePermission } from '@/hooks/useAuth'
 import { PERMISSIONS } from '@/constants/permissions'
 import type { Transaction } from '@/types'
-import { cn } from '@/utils/cn'
 import {
   accountBalances,
   buildLedgerRows,
